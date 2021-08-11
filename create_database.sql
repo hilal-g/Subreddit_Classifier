29574c89a0dac98ed739c241dc612bddfb6b0b01
hilal-g <hilalgen@gmail.com>
2021-08-11 20:30:10 +0300

    Create database and first table and gather list of subreddits

---
+++ create_database.sql
@@ -0,0 +1,12 @@
+
+-- Create the database called reddit_posts 
+CREATE DATABASE reddit_posts;
+
+-- Select reddit_posts database
+USE reddit_posts;
+
+-- Create table 
+CREATE TABLE reddit_dataset(id INT AUTO_INCREMENT,
+                            title MEDIUMTEXT,
+                            subreddit VARCHAR(255),
+                            PRIMARY KEY(id));
--- fetch_api.py
+++ fetch_api.py
@@ -1,8 +1,58 @@
 import json 
+import mysql.connector
 import pprint 
+import re
 import requests 
 
-response = requests.get("https://www.reddit.com/r/askreddit.json?limit=10", headers = {'User-agent': 'your bot 0.1'})
-data = response.json()
+from nltk import TweetTokenizer 
+from nltk.tokenize.treebank import TreebankWordDetokenizer 
 
-pprint.pprint(data)
+# Get the username and password 
+
+with open('../sc_auth.txt') as f:
+    lines = f.readlines()
+
+for i, line in enumerate(lines):
+    line = re.sub("\n", "", line)
+    lines[i] = line
+
+username = lines[0]
+password = lines[1]
+
+# Extract titles from chosen subreddits 
+
+subreddit_names = ["askhistorians",
+                    "writingprompts",
+                    "television",
+                    "explainlikeimfive",
+                    "lifeprotips",
+                    "relationship_advice",
+                    "science",
+                    "books",
+                    "nba",
+                    "philosophy"]
+
+titles = []
+
+for i in subreddit_names:
+    subreddit_name = i
+    response = requests.get("https://www.reddit.com/r/" + subreddit_name + ".json?limit=10", headers = {'User-agent': 'your bot 0.1'})
+
+    data = response.json()
+
+    sub_titles = []
+
+    for i in data['data']['children']:
+        title = TweetTokenizer().tokenize(i['data']['title'])
+        if 'selftext_html' in i['data']:
+            text = i['data']['selftext_html']
+        data = i['data']
+        sub_titles.append((TreebankWordDetokenizer().detokenize(title), subreddit_name))
+
+    sub_titles = sub_titles[2:]
+    titles.extend(sub_titles)
+
+pprint.pprint(titles)
+
+print(len(titles))
+
